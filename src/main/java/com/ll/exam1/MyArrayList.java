b26601f2a4efdd56eb2f8a480040a371a93fa3d9
youngjae459 <youngjae459@gmail.com>
2023-04-20 10:19:16 +0900

    feat: Object 타입으로 변경

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,10 +1,17 @@
 package com.ll.exam1;
 
 public class MyArrayList<T> {
-    private String[] data = new String[2];
+    public boolean debug = false;
+    private Object[] data;
     private int size = 0;
 
     MyArrayList() {
+        this(2);
+    }
+
+    // 가변 배열이지만 초기크기를 지정할 수 있도록
+    public MyArrayList(int dataLength) {
+        data = new Object[dataLength];
     }
 
     //리스트의 사이즈를 리턴
@@ -12,7 +19,7 @@ public class MyArrayList<T> {
         return size;
     }
 
-    public boolean add(String element) {
+    public boolean add(T element) {
         // 더할 때마다 공간을 추가해주어야 한다.
         // 데이터 공간이 부족하면 새 데이터 객체를 만든다.
         makeNewSpaceIfNotEnough();
@@ -31,11 +38,15 @@ public class MyArrayList<T> {
     }
 
     private void makeNewSpace() {
-        String[] newSpace = new String[data.length * 2]; // 기존
+        Object[] newSpace = new Object[data.length * 2]; // 기존
 
         // 기존 저장된 요소들을 전부 새로만든 공간에 옮긴다.
         for (int i = 0; i < data.length; i++) {
             newSpace[i] = data[i];
+        }
+
+        if (debug) {
+            System.out.printf("배열크기 증가 : %d => %d\n", data.length, newSpace.length);
         }
 
         data = newSpace;
@@ -45,7 +56,18 @@ public class MyArrayList<T> {
         return size >= data.length;
     }
 
-    public String get(int index) {
+    // 해당 위치의 원소를 반환
+    public Object get(int index) {
         return data[index];
+    }
+
+    public int indexOf(T element) {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(data[i])) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -1,15 +1,20 @@
 package com.ll.exam1;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.MethodName.class)
 class MyArrayListTest {
     @Test
     @DisplayName("testSize")
-    void t1() {
+    void t01() {
         MyArrayList<String> list = new MyArrayList<>();
         assertThat(list.size()).isEqualTo(0);
     }
@@ -17,7 +22,7 @@ class MyArrayListTest {
     // 추가기능 테스트
     @Test
     @DisplayName("add(\"사과\")")
-    void t2(){
+    void t2() {
         MyArrayList<String> list = new MyArrayList<>();
 
         list.add("사과");
@@ -28,7 +33,7 @@ class MyArrayListTest {
 
     @Test
     @DisplayName("get(\"포도\")")
-    void t3(){
+    void t03() {
         MyArrayList<String> list = new MyArrayList<>();
 
         list.add("사과");
@@ -40,23 +45,79 @@ class MyArrayListTest {
 
     @Test
     @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
-    void t4(){
+    void t04() {
         MyArrayList<String> list = new MyArrayList<>();
 
-        String[] data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(2);
+        // 초창기 배열의 길이
+        int dataLength1 = ((String[]) (TestUt.getFieldValue(list, "data", null))).length;
 
-        list.add("사과");
-        list.add("포도");
+        // IntStream.range(0, 10); // [0...9]
+        // 딱 1번 넘칠만큼의 데이터를 넣는다.
+        IntStream.range(0, dataLength1 + 1)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
 
-        data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(2);
+        int dataLength2 = ((String[]) (TestUt.getFieldValue(list, "data", null))).length;
+        assertThat(dataLength2).isGreaterThan(dataLength1);
+    }
 
-        list.add("당근");
+    @Test
+    @DisplayName("초기 data(배열)의 크기 설정")
+    void t05() {
+        // 초기 배열의 크기를 지정
+        MyArrayList<String> list = new MyArrayList<>(200);
 
-        data = TestUt.getFieldValue(list, "data", null);
-        assertThat(data.length).isEqualTo(4);
+        // 배열의 길이
+        int dataLength = ((String[]) (TestUt.getFieldValue(list, "data", null))).length;
 
-        assertThat(list.size()).isEqualTo(3);
+        assertThat(dataLength).isEqualTo(200);
+    }
+
+    @Test
+    @DisplayName("디버그 : 데이터를 많이 넣으면 배열증가가 많이 발생함")
+    void t06() {
+        MyArrayList<String> list = new MyArrayList<>();
+        list.debug = true;
+
+        // IntStream.range(0, 10); = [0, ... 9] 까지의 int 스트림 발생
+        // 딱 1번 넘칠만큼의 데이터를 넣는다.
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+    }
+
+    @Test
+    @DisplayName("디버그 : 초기 배열 크기 설정을 해줄 경우 배열이동이 줄어듬")
+    void t07() {
+        MyArrayList<String> list = new MyArrayList<>(25);
+        list.debug = true;
+
+        // IntStream.range(0, 10); = [0, ... 9] 까지의 int 스트림 발생
+        // 딱 1번 넘칠만큼의 데이터를 넣는다.
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+    }
+
+    @Test
+    @DisplayName("indexOf 테스트")
+    void t08() {
+        MyArrayList<String> list = new MyArrayList<>(100);
+
+        IntStream.range(0, 100)
+                .forEach(index -> list.add("사과 %d".formatted(index)));
+
+        assertThat(list.indexOf("사과 0")).isEqualTo(0);
+        assertThat(list.indexOf("사과 1")).isEqualTo(1);
+        assertThat(list.indexOf("사과 22")).isEqualTo(22);
+        assertThat(list.indexOf("사과 100")).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("다양한 타입을 받을 수 있도록 Object로 변경")
+    void t09() {
+        MyArrayList<Boolean> list = new MyArrayList<>();
+
+        list.add(true);
+        list.add(false);
+
+        assertThat(list.size()).isEqualTo(2);
     }
 }
